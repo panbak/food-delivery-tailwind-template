d9eeadf3c3043d5640c991abcfe26f9e5503155b
Panagiotis Bakas <panbak22@yahoo.gr>
2020-10-22 12:06:51 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  future: {},
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
